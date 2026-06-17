38be9117a0477c2417bc35da2100d249c2b305b7
Matheus Lula <matheusphillip170@gmail.com>
2026-06-17 10:37:45 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <h1>Welcome to EducAssist!</h1>
+      <p>Your personal educational assistant.</p>
+    </div>
+  )
+
+}
